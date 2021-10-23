c020f15f00f018e1df274ea8c5778d7bfa4cd22e
Sudhish Sepaul <sudhish.sepaul@gmail.com>
2021-10-23 17:27:14 -0500

    final changes made to documnets

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-/* Add your JavaScript to this file */
+window.onload = function(){
+    let sub = document.querySelector('button');
+    var statusDisplay = document.getElementsByClassName('message');
+    let form = document.getElementsByTagName('form');
+    
+        
+        sub.addEventListener('click', e => {
+            e.preventDefault();
+            console.log('click')
+            var email = document.getElementById('email').value;
+            console.log(email);
+            if(email == "")
+            {
+                console.log(statusDisplay);
+                statusDisplay[0].innerText = 'Please enter a valid email address';
+            }else{
+                statusDisplay[0].innerText = 'Thank you! Your email address '+ email + ' has been added to our mailing list!';
+            };
+            
+          });
+};
